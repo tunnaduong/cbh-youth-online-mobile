4b655f477e9a2d15334762f5df0e86e6faabf329
Duong Tung Anh <tunnaduong@gmail.com>
2024-12-17 20:29:10 +0700

    Enhance MenuScreen with logout functionality and user profile display; integrate action sheets for iOS and Android

--- app/screens/MainScreens/MenuScreen/index.js
+++ app/screens/MainScreens/MenuScreen/index.js
@@ -1,14 +1,41 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useEffect, useState } from "react";
-import { View, Text, ScrollView, Image } from "react-native";
+import React, { useEffect, useState, useContext } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Platform,
+  Alert,
+  ActionSheetIOS,
+} from "react-native";
+import { List } from "react-native-paper";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { CommonActions } from "@react-navigation/native";
 
-export default function MenuScreen() {
+export default function MenuScreen({ navigation }) {
   const [profileName, setProfileName] = useState("");
   const [username, setUsername] = useState("");
+  const { setIsLoggedIn } = useContext(AuthContext);
 
   const getData = async (key) => {
     const result = await AsyncStorage.getItem(key);
     return result;
+  };
+
+  const goToScreen = () => {
+    clearAll();
+    setIsLoggedIn(false);
+  };
+
+  const clearAll = async () => {
+    try {
+      await AsyncStorage.clear();
+    } catch (e) {
+      // clear error
+    }
   };
 
   useEffect(() => {
@@ -24,16 +51,189 @@ export default function MenuScreen() {
     fetchData();
   }, []);
 
+  const onPressIOS = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        title: "Bạn có chắc chắn muốn đăng xuất không?",
+        options: ["Hủy", "Đăng xuất"],
+        destructiveButtonIndex: 1,
+        cancelButtonIndex: 0,
+        userInterfaceStyle: "dark",
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          // cancel action
+        } else if (buttonIndex === 1) {
+          goToScreen();
+        }
+      }
+    );
+  };
+  const onPressAndroid = () =>
+    Alert.alert(
+      "Thông báo",
+      "Bạn có chắc chắn muốn đăng xuất không?",
+      [
+        {
+          text: "Có",
+          onPress: () => {
+            goToScreen();
+          },
+        },
+        {
+          text: "Không",
+          style: "cancel",
+        },
+      ],
+      { cancelable: true }
+    );
+
   return (
     <ScrollView>
-      <Image
-        source={{
-          uri: `https://api.chuyenbienhoa.com/v1.0/users/${username}/avatar`,
-        }}
-        style={{ width: 100, height: 100 }}
-      />
-      <Text>{profileName}</Text>
-      <Text>@{username}</Text>
+      <View className="mx-4 mt-4">
+        <TouchableOpacity className="gap-y-2">
+          <Image
+            source={{
+              uri: `https://api.chuyenbienhoa.com/v1.0/users/${username}/avatar`,
+            }}
+            style={{ width: 60, height: 60, borderRadius: 30 }}
+            className="border-2 border-gray-200"
+          />
+          <View>
+            <Text className="text-lg font-bold">{profileName}</Text>
+            <Text className="text-gray-500">@{username}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <List.Section>
+        <List.Subheader>Tài khoản</List.Subheader>
+        <List.Item
+          title="Trang cá nhân"
+          onPress={() => null}
+          right={() => (
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              style={{ marginRight: -10 }}
+            />
+          )}
+          left={() => (
+            <Ionicons
+              name="person-circle-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+            />
+          )}
+        />
+        <List.Item
+          title="Cài đặt"
+          onPress={() => null}
+          right={() => (
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              style={{ marginRight: -10 }}
+            />
+          )}
+          left={() => (
+            <Ionicons
+              name="settings-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+            />
+          )}
+        />
+        <List.Item
+          title="Đăng xuất"
+          onPress={() => {
+            Platform.OS === "ios" ? onPressIOS() : onPressAndroid();
+          }}
+          left={() => (
+            <Ionicons
+              name="log-out-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+              color={"#F66566"}
+            />
+          )}
+          titleStyle={{ color: "#F66566" }}
+        />
+      </List.Section>
+      <List.Section>
+        <List.Subheader>Hỗ trợ</List.Subheader>
+        <List.Item
+          title="Giới thiệu"
+          onPress={() => null}
+          right={() => (
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              style={{ marginRight: -10 }}
+            />
+          )}
+          left={() => (
+            <Ionicons
+              name="information-circle-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+            />
+          )}
+        />
+        <List.Item
+          title="Trợ giúp"
+          onPress={() => null}
+          right={() => (
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              style={{ marginRight: -10 }}
+            />
+          )}
+          left={() => (
+            <Ionicons
+              name="help-circle-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+            />
+          )}
+        />
+        <List.Item
+          title="Chính sách quyền riêng tư"
+          onPress={() => null}
+          right={() => (
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              style={{ marginRight: -10 }}
+            />
+          )}
+          left={() => (
+            <Ionicons
+              name="document-text-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+            />
+          )}
+        />
+        <List.Item
+          title="Điều khoản sử dụng"
+          onPress={() => null}
+          right={() => (
+            <Ionicons
+              name="chevron-forward-outline"
+              size={20}
+              style={{ marginRight: -10 }}
+            />
+          )}
+          left={() => (
+            <Ionicons
+              name="document-text-outline"
+              size={24}
+              style={{ marginLeft: 14, marginRight: -5 }}
+            />
+          )}
+        />
+      </List.Section>
     </ScrollView>
   );
 }
